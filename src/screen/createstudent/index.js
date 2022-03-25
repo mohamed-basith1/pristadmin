117daa063aa28e598eb736f8479bc35d4ebcfcf8
basith <basithjibri123@gmail.com>
2022-03-25 17:15:00 +0530

    table completed

--- src/screen/createstudent/index.js
+++ src/screen/createstudent/index.js
@@ -2,33 +2,70 @@ import { InputLabel, MenuItem, Select, TextField } from '@mui/material';
 import React, { useState } from 'react';
 import Button from '../../components/button';
 import './createstudent.css';
+import PersonPinIcon from '@mui/icons-material/PersonPin';
+import SchoolIcon from '@mui/icons-material/School';
+import { DataGrid } from '@mui/x-data-grid';
 const Createstudent = () => {
 	const [ year, setYear ] = useState('');
 	const [ department, setDepartment ] = useState('');
-	console.log(year);
+	const [ selection, setSelection ] = useState([]);
 
 	const formsubmit = () => {
 		console.log('submit clicked');
 	};
+	const columns = [
+		{ field: 'id', headerName: 'ID', width: 130 },
+		{ field: 'studentname', headerName: 'Student Name', width: 230 },
+		{ field: 'rollno', headerName: 'Roll no', width: 130 },
+		{ field: 'year', headerName: 'year', width: 230 },
+		{ field: 'department', headerName: 'Department', width: 230 }
+	];
+	const rows = [
+		{ id: 12343, studentname: 'Snow', rollno: 'Jon', year: 1, department: 'cse' },
+		{ id: 234, studentname: 'Lannister', rollno: 'Cersei', year: 1, department: 'cse' },
+		{ id: 333, studentname: 'Lannister', rollno: 'Jaime', year: 1, department: 'cse' },
+		{ id: 4443, studentname: 'Stark', rollno: 'Arya', year: 1, department: 'cse' },
+		{ id: 534, studentname: 'Targaryen', rollno: 'Daenerys', year: 2, department: 'cse' },
+		{ id: 634, studentname: 'Melisandre', rollno: null, year: 2, department: 'cse' },
+		{ id: 723, studentname: 'Clifford', rollno: 'Ferrara', year: 2, department: 'cse' },
+		{ id: 812, studentname: 'Frances', rollno: 'Rossini', year: 3, department: 'cse' },
+		{ id: 92, studentname: 'Roxie', rollno: 'Harvey', year: 4, department: 'cse' }
+	];
+
+	const testing = () => {
+		console.log('selected itemsssss', selection);
+	};
+
 	return (
 		<div className="studentcontainer">
 			<div className="leftside">
 				<div className="studentheader">
 					<div className="total">
+						<PersonPinIcon style={{ fontSize: 90 }} />
 						<h2>25</h2>
 						<p>Total students</p>
 					</div>
-					<div className="total">
-						<h2>4</h2>
+					<div className="total2">
+						<SchoolIcon style={{ fontSize: 90 }} />
+						<h2>5</h2>
 						<p>Total department</p>
 					</div>
 				</div>
-				<h2>Create Student</h2>
+				<h2 style={{ color: 'rgb(7,26,46)' }}>Create Student</h2>
 				<div className="createform">
 					<div className="input">
-						<TextField id="outlined-basic" label="Enter Name" variant="outlined" />
-						<TextField id="outlined-basic" label="Enter Roll no" variant="outlined" />
-						<TextField id="outlined-basic" label="Enter password" variant="outlined" />
+						<div>
+							<InputLabel id="demo-simple-select-label">Enter Student Name</InputLabel>
+							<TextField id="outlined-basic" label="Enter Name" variant="outlined" />
+						</div>
+						<div>
+							<InputLabel id="demo-simple-select-label">Enter Student Roll no</InputLabel>
+							<TextField id="outlined-basic" label="Enter Roll no" variant="outlined" />
+						</div>
+						<div>
+							<InputLabel id="demo-simple-select-label">Enter password</InputLabel>
+							<TextField id="outlined-basic" label="Enter password" variant="outlined" />
+						</div>
 					</div>
 					<div className="selects">
 						<InputLabel id="demo-simple-select-label">year</InputLabel>
@@ -65,7 +102,77 @@ const Createstudent = () => {
 				</div>
 				<Button height={40} width={'100%'} action={() => formsubmit()} name={'Submit'} />
 			</div>
-			<div className="rightside">right side</div>
+			<div className="rightside">
+				<div className="tablecontainer">
+					<h2 style={{ color: 'rgb(7,26,46)' }}>All Students</h2>
+					<div className="table">
+						<DataGrid
+							rows={rows}
+							columns={columns}
+							checkboxSelection
+							hideFooterPagination
+							onSelectionModelChange={(e) => {
+								setSelection(e);
+							}}
+							options={{ selection: true }}
+						/>
+					</div>
+				</div>
+				<div className="editingcontainer">
+					<h2 style={{ color: 'rgb(7,26,46)' }}>Edit Section</h2>
+					<p style={{ color: 'rgba(7,26,46,.5)' }}>Total selection :</p>
+					<div className="editing">
+						<div className="editbox">
+							<p style={{ fontSize: 15, fontWeight: 500, color: 'rgb(7,26,46)' }}>Year</p>
+							<Select
+								labelId="demo-simple-select-autowidth-label"
+								id="demo-simple-select-autowidth"
+								value={year}
+								onChange={(e) => setYear(e.target.value)}
+								autoWidth
+								label="select year"
+							>
+								<MenuItem value={1}>1st year</MenuItem>
+								<MenuItem value={2}>2nd year</MenuItem>
+								<MenuItem value={3}>3rd year</MenuItem>
+								<MenuItem value={4}>4th year</MenuItem>
+							</Select>
+							<Button height={30} width={'100%'} action={() => formsubmit()} name={'Change'} />
+						</div>
+
+						<div className="editbox">
+							<p style={{ fontSize: 15, fontWeight: 500, color: 'rgb(7,26,46)' }}>Department</p>
+							<Select
+								labelId="demo-simple-select-autowidth-label"
+								id="demo-simple-select-autowidth"
+								value={department}
+								onChange={(e) => setDepartment(e.target.value)}
+								autoWidth
+								label="select department"
+							>
+								<MenuItem value={'cse'}>CSE</MenuItem>
+								<MenuItem value={'mechanical'}>MECH</MenuItem>
+								<MenuItem value={'ece'}>ECE</MenuItem>
+								<MenuItem value={'eee'}>EEE</MenuItem>
+								<MenuItem value={'civil'}>CIVIL</MenuItem>
+							</Select>
+							<Button height={30} width={'100%'} action={() => formsubmit()} name={'Change'} />
+						</div>
+
+						<div className="editbox">
+							<p style={{ fontSize: 15, fontWeight: 500, color: 'rgb(7,26,46)' }}>Roll no</p>
+							<TextField id="outlined-basic" label="Enter Roll no" variant="outlined" />
+							<Button height={30} width={'100%'} action={() => formsubmit()} name={'Change'} />
+						</div>
+
+						<div className="editbox">
+							<p style={{ fontSize: 15, fontWeight: 500, color: 'rgb(7,26,46)' }}>Name</p>
+							<TextField id="outlined-basic" label="Enter Roll no" variant="outlined" />
+							<Button height={30} width={'100%'} action={() => formsubmit()} name={'Change'} />
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
