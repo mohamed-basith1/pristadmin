890f3d4dadcb00e027ad0683c7e1eb294d9e44d0
basith <basithjibri123@gmail.com>
2022-03-28 14:41:15 +0530

    result

--- src/screen/result/index.js
+++ src/screen/result/index.js
@@ -1,7 +1,27 @@
+import { TextField } from '@mui/material';
 import React from 'react';
-
+import './result.css'
 const Result = () => {
-	return <div>Result</div>;
+	return (<div className='resultcontainer'>
+<div className="resultleft">
+	<h2 style={{color:"rgb(7,26,46)"}}>Upload Result</h2>
+	<div className="resultform">
+
+	
+<div className='resulttitle'>
+<p>Title</p>
+<TextField id="outlined-basic" label="Enter Name" variant="outlined"  style={{width:"100%"}}/>
+</div>
+<div className='resulttitle'>
+<p>Description</p>
+<TextField id="outlined-basic" label="Enter Name" variant="outlined"  style={{width:"100%"}}/>
+</div>
+</div>
+	
+	<p>Ti</p>
+</div>
+<div className="resultright">right</div>
+	</div>)
 };
 
 export default Result;
