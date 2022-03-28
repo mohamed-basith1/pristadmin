65862ff79cc3592063e2e460ea59281b865e796b
basith <basithjibri123@gmail.com>
2022-03-28 14:57:16 +0530

    submit button

--- src/screen/result/index.js
+++ src/screen/result/index.js
@@ -1,7 +1,12 @@
 import { TextField } from '@mui/material';
 import React from 'react';
 import './result.css'
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import Button from '../../components/button';
 const Result = () => {
+	const formsubmit=()=>{
+		console.log("hi")
+	}
 	return (<div className='resultcontainer'>
 <div className="resultleft">
 	<h2 style={{color:"rgb(7,26,46)"}}>Upload Result</h2>
@@ -16,9 +21,25 @@ const Result = () => {
 <p>Description</p>
 <TextField id="outlined-basic" label="Enter Name" variant="outlined"  style={{width:"100%"}}/>
 </div>
-</div>
+
+
+<div className='uploadcontainer'>
+	<CloudUploadIcon style={{fontSize:60}} />
 	
-	<p>Ti</p>
+	Upload Result
+	
+	</div>
+	<Button
+						height={40}
+						width={'80%'}
+						action={() => formsubmit()}
+						color={'rgba(21, 122, 255,.9)'}
+						name={'Submit'}
+						textcolor={'white'}
+					/>
+</div>
+
+	
 </div>
 <div className="resultright">right</div>
 	</div>)
